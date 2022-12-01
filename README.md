23673d85511465e6c1fc3e05f56972183717e9ef
KTH-96 <taehyun960@gmail.com>
2022-12-01 19:47:33 +0900

    init build

---
+++ README.md
@@ -0,0 +1,7 @@
+## Java, Spring으로 만드는 카카오톡 알람서비스
+개인 프로젝트 제작용
+____
+### 사용 기술 및 환경
+- java 11.0.13
+- spring boot version 2.7.6
+- gradle 7.5.1
---
+++ src/test/java/com/myproject/alarm/AlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.alarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
